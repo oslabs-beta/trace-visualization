42843abe32a67224101c4bc4ad7484e402a015f1
Kevin-Featherstone <117868278+Kevin-Featherstone@users.noreply.github.com>
2023-07-04 16:54:44 -0500

    moved socketio server from extension.ts to react client
    added an h1 tag above the webview component to display data being pushed by socketio during testing

--- pern_src/client/src/App.tsx
+++ pern_src/client/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Webview from './Webview';
+import socket from './socket-connection';
 
 function App() {
+
+	const [socketId, setSocketId] = useState('');
+	const [stackData, setStackData] = useState({});
+
+	useEffect(()=> {
+		socket.on('connect', () => {
+			setSocketId(socket.id);
+			socket.emit('socketId', {data: socket.id});
+			console.log('new connection: ', `id ${socket.id}: `, socket);
+		});
+		
+		socket.on('interaction', (data) => {
+			setStackData(data);
+		});
+	
+		socket.on('disconnect', () => {
+			console.log(`id ${socketId} disconnected`);
+		})
+	},[]);
+
+
 	return (
 		<div className="webview">
+			<h1>{JSON.stringify(stackData)}</h1>
 			<Webview />
 		</div>
 	);
