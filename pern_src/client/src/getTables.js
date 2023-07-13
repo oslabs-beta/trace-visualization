edd206dc9112004b9b84d10a8d9692d607d45b68
scottdeutsch40 <scottdeutsch40@gmail.com>
2023-07-12 20:28:11 -0400

    7/12

--- pern_src/client/src/getTables.js
+++ pern_src/client/src/getTables.js
@@ -12,27 +12,29 @@ const query = (text, params, callback) => {
 }
 
 const getDatabaseInfo = async () => {
-const queryText = "SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_type = \'BASE TABLE'"
+  const queryText = "SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_type = \'BASE TABLE'"
 
-// const queryText = 'SELECT * FROM stocks'
-const data = await query(queryText);
+  // const queryText = 'SELECT * FROM stocks'
+  const data = await query(queryText);
 
-const ourTables = [];
-for (let table of data.rows)
-    if (!table['table_name'].includes('pg_') && !table['table_name'].includes('sql_')){
-        ourTables.push(table['table_name'])
-    }
-
-console.log(ourTables)
-
-const tablesInfo = {};
-
-for (let table of ourTables){
-    const columnQuery = `SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ${table}`
-const data = await query(queryText);
-}
+  const ourTables = [];
+  for (let table of data.rows)
+      if (!table['table_name'].includes('pg_') && !table['table_name'].includes('sql_')){
+          ourTables.push(table['table_name'])
+      }
 
 
+  const tablesInfo = {};
+
+  for (let table of ourTables){
+      tablesInfo[table] = [];
+      const columnQuery = 'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = $1'
+      const data = await query(columnQuery, [table]);
+      for (eachTable of data.rows){
+        tablesInfo[table].push(eachTable['column_name'])
+      }
+  }
+    return tablesInfo;
 }
 
 getDatabaseInfo();
