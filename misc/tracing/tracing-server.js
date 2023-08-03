a3d02629410d5488c954e7600923d95ceea1059f
Kevin-Featherstone <117868278+Kevin-Featherstone@users.noreply.github.com>
2023-08-02 21:21:34 -0500

    performance improvements

--- misc/tracing/tracing-server.js
+++ misc/tracing/tracing-server.js
@@ -1,12 +1,16 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { PgInstrumentation } = require('@opentelemetry/instrumentation-pg');
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
-//Open Telemetry Auto Instrumentation
-const sdk = new NodeSDK({
-	serviceName: 'my-app',
-	traceExporter: new OTLPTraceExporter({ url: 'http://localhost:12720/v1/traces' }),
-	instrumentations: [new HttpInstrumentation(), new PgInstrumentation()],
+registerInstrumentations({
+	instrumentations: [new HttpInstrumentation(), new PgInstrumentation()]
 });
-sdk.start();
+
+const provider = new NodeTracerProvider();
+const exporter = new OTLPTraceExporter({url: 'http://localhost:12720/v1/traces'});
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+provider.register();
